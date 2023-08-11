20072beeee292660c743a62e43874a540d8a5b2a
Daniel Zoba <daniel.sobe@sorben.com>
2023-08-11 13:24:49 +0200

    try quantized model

--- startme.sh
+++ startme.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-VOSK_SAMPLE_RATE=48000 /vosk_whisper_server 0.0.0.0 2700 1 /uasr-data/ggml-model.bin
+# VOSK_SAMPLE_RATE=48000 /vosk_whisper_server 0.0.0.0 2700 1 /uasr-data/ggml-model.bin
+VOSK_SAMPLE_RATE=48000 /vosk_whisper_server 0.0.0.0 2700 1 /uasr-data/ggml-model-q5_0.bin
