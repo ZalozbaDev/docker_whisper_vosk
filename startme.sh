80796cbc226cc7579483be4edf6f570934d458fd
Daniel Zoba <daniel.sobe@sorben.com>
2023-12-07 13:01:27 +0100

    remember how to switch off annoying GDB prints

--- startme.sh
+++ startme.sh
@@ -18,4 +18,5 @@ while /bin/true; do sleep 1 ; done
 # export VOSK_SAMPLE_RATE=48000 
 # gdb /vosk_whisper_server
 # set args 0.0.0.0 2700 1 /whisper/Korla/hsb_stt_demo/hsb_whisper/ggml-model.q8_0.bin
+# set print thread-events off
 # run
